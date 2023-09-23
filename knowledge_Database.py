7d366a7bd887358b10e766c9908bb734637c6f69
steventut <132977528+steventut@users.noreply.github.com>
2023-09-24 00:30:42 +0800

    Update knowledge_Database.py

--- knowledge_Database.py
+++ knowledge_Database.py
@@ -6,14 +6,17 @@ openai.api_key = st.secrets['api']
 #context = []
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-    )
-#   print(str(response.choices[0].message))
-    return response.choices[0].message["content"]
-
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature, # this is the degree of randomness of the model's output
+        )
+    #   print(str(response.choices[0].message))
+        return response.choices[0].message["content"]
+    except:
+        return response.choices[0].message["content"]
+        
 def collect_messages(prompt):
     #global context
     #prompt = inp.value_input

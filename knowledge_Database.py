fc4d97f2123e7d275cee109ba3d1de4af0f6fdec
steventut <132977528+steventut@users.noreply.github.com>
2023-09-24 01:24:07 +0800

    Update knowledge_Database.py

--- knowledge_Database.py
+++ knowledge_Database.py
@@ -15,7 +15,7 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
     #   print(str(response.choices[0].message))
         return response.choices[0].message["content"]
     except:
-        return openai.error.RateLimitError
+        return openai.error.RateLimitError()
         #return response.choices[0].message["content"]
         
 def collect_messages(prompt):

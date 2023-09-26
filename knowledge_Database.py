83c074eec45a62e1bff3604235dfa2b23512bef4
steventut <132977528+steventut@users.noreply.github.com>
2023-09-26 14:48:21 +0800

    Update knowledge_Database.py

--- knowledge_Database.py
+++ knowledge_Database.py
@@ -17,6 +17,7 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
         return response.choices[0].message["content"]
     except:
         st.session_state.isOpenAiAPIError = True
+        st.session_state.isOpenAiAPIErrorEver = True
         return openai.error.RateLimitError()
         #return response.choices[0].message["content"]
         

a348c6bc90a94a16ba7a94be0cd64daf40cb8c5b
steventut <132977528+steventut@users.noreply.github.com>
2023-09-26 13:16:49 +0800

    Update knowledge_Database.py

--- knowledge_Database.py
+++ knowledge_Database.py
@@ -17,7 +17,8 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
         return response.choices[0].message["content"]
     except:
         st.session_state.isOpenAiAPIError = True
-        return openai.error.RateLimitError()
+        LoadPizzaResturant('')
+        #return openai.error.RateLimitError()
         #return response.choices[0].message["content"]
         
 def collect_messages(prompt):

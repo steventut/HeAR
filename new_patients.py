17bbf079753ae583195f39bc5188970e600578fe
steventut <132977528+steventut@users.noreply.github.com>
2026-02-17 15:39:53 +0800

    Update new_patients.py

--- new_patients.py
+++ new_patients.py
@@ -42,6 +42,48 @@ def Load_HeAR_model():
   st.write("✅ AI Ready.")
   return loaded_model
 
+#Cell 3: Sample codes from Huggin Face to use google/HeAR (1)
+SAMPLE_RATE = 16000  # Samples per second (Hz)
+CLIP_DURATION = 2    # Duration of the audio clip in seconds
+##CLIP_DURATION = 3    ## Steve: try 3 seconds: AI distances were changed! BUT did not aligh with motor_UPDRS scores!!!
+CLIP_LENGTH = SAMPLE_RATE * CLIP_DURATION  # Total number of samples
+
+#Cell 4: Sample codes from Huggin Face to use google/HeAR (2)
+import numpy as np
+from scipy.io import wavfile
+from scipy import signal
+from IPython.display import Audio, display
+
+def resample_audio_and_convert_to_mono(
+  audio_array: np.ndarray,
+  sampling_rate: float,
+  new_sampling_rate: float = SAMPLE_RATE,
+  ) -> np.ndarray:
+  """
+  Resamples an audio array to 16kHz and converts it to mono if it has multiple channels.
+
+  Args:
+    audio_array: A numpy array representing the audio data.
+    sampling_rate: The original sampling rate of the audio.
+    new_sampling_rate: Target sampling rate.
+
+  Returns:
+    resampled_audio_mono: A numpy array representing the resampled mono audio at 16kHz.
+  """
+  # Convert to mono if it's multi-channel
+  if audio_array.ndim > 1:
+    audio_mono = np.mean(audio_array, axis=1)
+  else:
+    audio_mono = audio_array
+
+  # Resample
+  original_sample_count = audio_mono.shape[0]
+  new_sample_count = int(round(original_sample_count * (new_sampling_rate / sampling_rate)))
+  resampled_audio_mono = signal.resample(audio_mono, new_sample_count)
+
+  return resampled_audio_mono
+
+###### HeAR: new_patients
 def new_patients():
   # --- 4. PROCESS NEW PERSON ---
   session_new.demo_stage = "Ahh: Capturing Voice Biomarker" #New patient comeing in for healthy and PD progression check
@@ -50,5 +92,6 @@ def new_patients():
   st.write (f"--- 🔍 Analyzing New Input: new_person_file ---")
   new_person_file = "recording.wav"  #"/content/Healthy/VA1GGIAORVG47F300320171212.wav"
   st.write("⏳ Processing new patient's embedding vectors...")
-  #session_new.new_vecs.append(get_embedding(new_person_file)) #new_vecs
-  #session_new.new_labels.append(datetime.now().strftime("%Y-%m-%d\n%H:%M")) #new_labels
+  session_new.new_vecs.append(get_embedding(new_person_file)) #new_vecs
+  st.write(session_new.new_vecs)
+  session_new.new_labels.append(datetime.now().strftime("%Y-%m-%d\n%H:%M")) #new_labels

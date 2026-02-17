8bebba2eefe0eeffe7e5ad5029c24139d15c1146
steventut <132977528+steventut@users.noreply.github.com>
2026-02-17 14:34:18 +0800

    Update new_patients.py

--- new_patients.py
+++ new_patients.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 session_new = st.session_state  #keep new patients' 6 info
 if 'new_vecs' not in session_state_new:
+  session_new.demo_stage = "Baseline Model"
   session_new.new_vecs = []
   session_new.new_labels = []
   session_new.new_distances = []
@@ -12,10 +13,27 @@ if 'new_vecs' not in session_state_new:
   session_new.new_shimmers = []
   session_new.new_updrs_scores = []
 
+def Login_huggingface_and_Load_HeAR_model()
+  #Cell 2: Login to Huggin Face
+  from huggingface_hub.utils import HfFolder
+  
+  if HfFolder.get_token() is None:
+      from huggingface_hub import notebook_login
+      notebook_login()
+  # Cell 5: Load google/HeAR Model
+  from huggingface_hub import from_pretrained_keras
+  
+  # Load the model directly from Hugging Face Hub
+  loaded_model = from_pretrained_keras("google/hear")
+  st.write("✅ AI Ready.")
+  return loaded_model
+
 def new_patients():
   # --- 4. PROCESS NEW PERSON ---
+  session_new.demo_stage = "Ahh: Capturing Voice Biomarker" #New patient comeing in for healthy and PD progression check
+  Login_huggingface_and_Load_HeAR_model()
   st.write (f"--- 🔍 Analyzing New Input: new_person_file ---")
   new_person_file = "recording.wav"  #"/content/Healthy/VA1GGIAORVG47F300320171212.wav"
   st.write("⏳ Processing new patient's embedding vectors...")
-  session_new.new_vecs.append(get_embedding(new_person_file)) #new_vecs
-  session_new.new_labels.append(datetime.now().strftime("%Y-%m-%d\n%H:%M")) #new_labels
+  #session_new.new_vecs.append(get_embedding(new_person_file)) #new_vecs
+  #session_new.new_labels.append(datetime.now().strftime("%Y-%m-%d\n%H:%M")) #new_labels

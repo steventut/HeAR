e2181ecbe4201043ae643b7edef0e92db1a2fdfe
steventut <132977528+steventut@users.noreply.github.com>
2026-02-17 16:07:10 +0800

    Create try_recording_st_audiorec.py

---
+++ try_recording_st_audiorec.py
@@ -0,0 +1,17 @@
+# recording.py
+import streamlit as st
+from st_audiorec import st_audiorec
+
+def recording():
+    st.title("🎙️ Voice Recorder")
+    
+    # Record audio
+    wav_audio_data = st_audiorec()
+    
+    if wav_audio_data is not None:
+        # Save directly to file
+        with open("recording.wav", "wb") as f:
+            f.write(wav_audio_data)
+        
+        st.success("✅ Saved: recording.wav")
+        st.audio(wav_audio_data, format='audio/wav')

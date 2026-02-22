937ddef8ace0155aec51cd957998909b8b3e19ab
steventut <132977528+steventut@users.noreply.github.com>
2026-02-23 02:50:07 +0800

    Update km_app.py

--- km_app.py
+++ km_app.py
@@ -161,10 +161,10 @@ def load_data():
         session_new.new_distances = []
         for vecs in session_new.new_vecs:
             session_new.new_distances.append(euclidean(vecs, golden_vector)) #new_distances	
-    if len(session_new.new_distances) == 0:
-        st.write("New patient's AI distance: ", round(session_new.new_distances[0]))
-    else:
-        st.write("New patient's AI distance: ", round(session_new.new_distances[1]))
+        if len(session_new.new_distances) == 0:
+            st.write("New patient's AI distance: ", round(session_new.new_distances[0]))
+        else:
+            st.write("New patient's AI distance: ", round(session_new.new_distances[1]))
 
     # Demo 1: show baseline model (no new patient)
     # --- 5. GENERATE PLOT 1: UPDATED PCA MAP ---
